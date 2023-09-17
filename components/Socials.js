399da30cdbf174986dfec89d24887368e346ab5d
ibrahzizo360 <92813019+ibrahzizo360@users.noreply.github.com>
2023-09-17 19:15:37 +0000

    added socials

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,24 @@
+import Link from "next/link";
+import { RiInstagramLine, RiLinkedinLine, RiYoutubeLine } from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className="flex items-center gap-x-5 text-lg">
+    <Link href={''}  className="hover:text-accent transition-all duration-300">
+      <RiYoutubeLine />
+    </Link>
+    <Link href={''}  className="hover:text-accent transition-all duration-300">
+      <RiInstagramLine />
+    </Link>
+    <Link href={''}  className="hover:text-accent transition-all duration-300">
+      <RiLinkedinLine />
+    </Link>
+    <Link href={''}  className="hover:text-accent transition-all duration-300">
+      <RiYoutubeLine />
+    </Link>
+    <Link href={''}  className="hover:text-accent transition-all duration-300">
+      <RiYoutubeLine />
+    </Link>
+  </div>;
 };
 
 export default Socials;
