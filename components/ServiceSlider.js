a88e919f9f5b57e31d64c3c952257f497736eed2
ibrahzizo360 <92813019+ibrahzizo360@users.noreply.github.com>
2023-09-19 14:30:07 +0000

    added slider at services page

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,3 +1,15 @@
+//import swiper react componenets
+import {Swiper, SwiperSlide} from 'swiper/react';
+
+//import swiper  styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import "swiper/swiper-bundle.css";
+
+// import required modules
+import { FreeMode, Pagination } from 'swiper';
+
 // icons
 import {
   RxCrop,
@@ -5,7 +17,12 @@ import {
   RxDesktop,
   RxReader,
   RxRocket,
+  RxArrowTopRight,
 } from "react-icons/rx";
+
+//framer motion
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
 
 
 // data
@@ -38,7 +55,45 @@ const serviceData = [
 ];
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return <Swiper breakpoints={{
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 15,
+    },
+
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+  }}
+  freeMode={true}
+  pagination={{
+    clickable : true,
+  }}
+  modules={{FreeMode, Pagination}}
+  className='h-[240px] sm:h-[340px]'
+  >
+    {
+      serviceData.map((item, index) => {
+        return <SwiperSlide key={index}>
+          <div className='bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8
+          flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer'>
+            {/* icon */}
+            <div>{item.icon}</div>
+            {/* title and description */}
+            <div>
+              <div>{item.title}</div>
+              <p>{item.description}</p>
+            </div>
+            {/* arrow */}
+            <div className='text-3xl'>
+              <RxArrowTopRight />
+            </div>
+          </div>
+        </SwiperSlide>
+      })
+    }
+  </Swiper>;
 };
 
 export default ServiceSlider;
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,31 @@
+import {motion} from 'framer-motion'
+import Circles from '../../components/Circles';
+import Bulb from '../../components/Bulb'
+import ServiceSlider from '../../components/ServiceSlider'
+
 const Services = () => {
-  return <div>Services</div>;
+  return <div className='h-full bg-primary/30 py-36 flex items-center'>
+    <Circles />
+    <div className='container mx-auto'>
+      <div className='flex flex-col xl:flex-row gap-x-8'>
+        {/* text */}
+        <div className='text-center flex xl:w-[30vw] flex-col lg:text-left 
+        mb-4 xl:mb-0'>
+          <h2 className='h2 xl:mt-8'>
+            My Services <span className='text-accent'>.</span>
+          </h2>
+          <p className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+          lorem-ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam
+          lorem-ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam
+          lorem-ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam
+          </p>
+        </div>
+        {/* slider */}
+        <ServiceSlider />
+      </div>
+    </div>
+    <Bulb />
+  </div>;
 };
 
 export default Services;
