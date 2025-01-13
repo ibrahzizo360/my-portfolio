331e54fc590153a0d09a5057e9a42c4dc5962656
ibrahzizo360 <ibrahimaziz200000@gmail.com>
2025-01-13 00:40:19 +0000

    minor fix

--- components/Socials.js
+++ components/Socials.js
@@ -1,18 +1,38 @@
 import Link from "next/link";
-import { RiInstagramLine, RiLinkedinLine, RiWhatsappLine, RiYoutubeLine } from "react-icons/ri";
+import {
+  RiInstagramLine,
+  RiLinkedinLine,
+  RiWhatsappLine,
+  RiYoutubeLine,
+} from "react-icons/ri";
 
 const Socials = () => {
-  return <div className="flex items-center gap-x-5 text-lg">
-    <Link href={'https://api.whatsapp.com/send/?phone=%2B233509438082&text&type=phone_number'}  className="hover:text-accent transition-all duration-300">
-      <RiWhatsappLine />
-    </Link>
-    <Link href={'https://www.instagram.com/the_ohene/?utm_source=qr&igsh=ZXpkd3Jjb3FiNGdv'}  className="hover:text-accent transition-all duration-300">
-      <RiInstagramLine />
-    </Link>
-    <Link href={'www.linkedin.com/in/ohene-agyekum'}  className="hover:text-accent transition-all duration-300">
-      <RiLinkedinLine />
-    </Link>
-  </div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link
+        href={
+          "https://api.whatsapp.com/send/?phone=%2B233509438082&text&type=phone_number"
+        }
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiWhatsappLine />
+      </Link>
+      <Link
+        href={
+          "https://www.instagram.com/the_ohene/?utm_source=qr&igsh=ZXpkd3Jjb3FiNGdv"
+        }
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiInstagramLine />
+      </Link>
+      <Link
+        href={"https://www.linkedin.com/in/ohene-agyekum"}
+        className="hover:text-accent transition-all duration-300"
+      >
+        <RiLinkedinLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
